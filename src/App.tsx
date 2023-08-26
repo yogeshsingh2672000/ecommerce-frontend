7273347f0a5b6b8eb0630c41911df6c079dacc85
Yogesh Kumar <yogeshsingh2672000@gmail.com>
2023-08-26 14:28:04 +0530

    minimise all product api call

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import NoPage from "./pages/NoPage";
 import Contact from "./pages/Contact";
 import Layout from "./Layout/Layout";
+import { getData } from "./hooks/fetchProduct";
 
 function App() {
+  const [products, setProducts] = useState<any>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getData();
+        setProducts(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
           <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
+            <Route index element={<Home products={products} />} />
             <Route path="contact" element={<Contact />} />
             <Route path="*" element={<NoPage />} />
           </Route>
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,22 +1,8 @@
-import React, { useState, useEffect } from "react";
-import { getData } from "../hooks/fetchProduct";
+import React from "react";
 import Card from "../library/Card";
 
-function Home() {
-  const [products, setProducts] = useState<any>(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const data = await getData();
-        setProducts(data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-
-    fetchData();
-  }, []);
+function Home(props: any) {
+  const { products } = props;
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center py-4 gap-8 px-2">
