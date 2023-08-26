6b3f9aa79f21291dec1dad6c7709c512c8065c9e
Yogesh Kumar <yogeshsingh2672000@gmail.com>
2023-08-26 15:51:06 +0530

    add pagination

--- src/hooks/fetchProduct.ts
+++ src/hooks/fetchProduct.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
-const getData = async () => {
-  const data = await axios.get("http://localhost:3003/api/product/all");
+const getData = async (page: number = 0) => {
+  const data = await axios.get(
+    `http://localhost:3003/api/product/all?page=${page}`
+  );
   return data.data;
 };
 
